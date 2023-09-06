dbb81a8844d63be32d3d7e1e8d7316b627cef996
Emmanuel Balogun <80977358+DreXuri@users.noreply.github.com>
2023-09-06 13:24:59 +0100

    freezed package generation

---
+++ lib/model/note_model.freezed.dart
@@ -0,0 +1,198 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'note_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Notes _$NotesFromJson(Map<String, dynamic> json) {
+  return _Notes.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Notes {
+  @JsonKey(name: 'id')
+  int get id => throw _privateConstructorUsedError;
+  @JsonKey(name: 'title')
+  String get title => throw _privateConstructorUsedError;
+  @JsonKey(name: 'description')
+  String get description => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $NotesCopyWith<Notes> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NotesCopyWith<$Res> {
+  factory $NotesCopyWith(Notes value, $Res Function(Notes) then) =
+      _$NotesCopyWithImpl<$Res, Notes>;
+  @useResult
+  $Res call(
+      {@JsonKey(name: 'id') int id,
+      @JsonKey(name: 'title') String title,
+      @JsonKey(name: 'description') String description});
+}
+
+/// @nodoc
+class _$NotesCopyWithImpl<$Res, $Val extends Notes>
+    implements $NotesCopyWith<$Res> {
+  _$NotesCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? description = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_NotesCopyWith<$Res> implements $NotesCopyWith<$Res> {
+  factory _$$_NotesCopyWith(_$_Notes value, $Res Function(_$_Notes) then) =
+      __$$_NotesCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {@JsonKey(name: 'id') int id,
+      @JsonKey(name: 'title') String title,
+      @JsonKey(name: 'description') String description});
+}
+
+/// @nodoc
+class __$$_NotesCopyWithImpl<$Res> extends _$NotesCopyWithImpl<$Res, _$_Notes>
+    implements _$$_NotesCopyWith<$Res> {
+  __$$_NotesCopyWithImpl(_$_Notes _value, $Res Function(_$_Notes) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? description = null,
+  }) {
+    return _then(_$_Notes(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Notes implements _Notes {
+  const _$_Notes(
+      {@JsonKey(name: 'id') this.id = 0,
+      @JsonKey(name: 'title') this.title = '',
+      @JsonKey(name: 'description') this.description = ''});
+
+  factory _$_Notes.fromJson(Map<String, dynamic> json) =>
+      _$$_NotesFromJson(json);
+
+  @override
+  @JsonKey(name: 'id')
+  final int id;
+  @override
+  @JsonKey(name: 'title')
+  final String title;
+  @override
+  @JsonKey(name: 'description')
+  final String description;
+
+  @override
+  String toString() {
+    return 'Notes(id: $id, title: $title, description: $description)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Notes &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.description, description) ||
+                other.description == description));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, title, description);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_NotesCopyWith<_$_Notes> get copyWith =>
+      __$$_NotesCopyWithImpl<_$_Notes>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_NotesToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Notes implements Notes {
+  const factory _Notes(
+      {@JsonKey(name: 'id') final int id,
+      @JsonKey(name: 'title') final String title,
+      @JsonKey(name: 'description') final String description}) = _$_Notes;
+
+  factory _Notes.fromJson(Map<String, dynamic> json) = _$_Notes.fromJson;
+
+  @override
+  @JsonKey(name: 'id')
+  int get id;
+  @override
+  @JsonKey(name: 'title')
+  String get title;
+  @override
+  @JsonKey(name: 'description')
+  String get description;
+  @override
+  @JsonKey(ignore: true)
+  _$$_NotesCopyWith<_$_Notes> get copyWith =>
+      throw _privateConstructorUsedError;
+}
